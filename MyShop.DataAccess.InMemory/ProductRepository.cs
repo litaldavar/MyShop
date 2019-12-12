19c89ea27f18a5a3901b68f61cdbce14c56de030
litaldavar <lital.soder@gmail.com>
2019-12-12 14:00:18 +0200

    Modals and storage

--- MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -11,8 +11,73 @@ namespace MyShop.DataAccess.InMemory
     public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
-        List<Product> products = new List<Product>();
+        List<Product> products; 
 
+        public ProductRepository()
+        {
+            products = cache["Products"] as List<Product>;
 
+            if(products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["Products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if(productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found!");
+            }
+        }
+
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found!");
+            }
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable(); 
+        }
+
+        public void Delete(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+
+            if (product != null)
+            {
+                products.Remove(product);
+            }
+            else
+            {
+                throw new Exception("Product not found!");
+            }
+        }
     }
 }
