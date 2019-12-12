3cc114a2be4d8d6bd4dfbc9c315f9555e0e312a7
litaldavar <lital.soder@gmail.com>
2019-12-12 11:42:29 +0200

    1.baseProject fitcher setup

---
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Modals;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+
+    }
+}
