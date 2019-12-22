709546d0b150d4c1422a418312f0dbf06f2287f6
litaldavar <lital.soder@gmail.com>
2019-12-22 14:54:39 +0200

    Added Basket Ui

---
+++ MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using MyShop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        BasketService BasketService;
+
+        public BasketController(BasketService basketService)
+        {
+            this.BasketService = basketService;
+        }
+        // GET: Basket
+        public ActionResult Index()
+        {
+            var model = BasketService.GetBasketItems(this.HttpContext);
+            return View(model);
+        }
+
+        public ActionResult AddToBasket(string Id)
+        {
+            BasketService.AddToBasket(this.HttpContext, Id);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromBasket(string Id)
+        {
+            BasketService.RemoveFromBasket(this.HttpContext, Id);
+
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult BasketSummary()
+        {
+            var BasketSummary = BasketService.GetBasketSammary(this.HttpContext);
+            return PartialView(BasketSummary);
+        }
+    }
+}
