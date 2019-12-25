602831ad22d08885f290106ad9713cdfeed9d92a
litaldavar <lital.soder@gmail.com>
2019-12-25 17:29:42 +0200

    Added Unit Testing

---
+++ MyShop.WebUI.Tests/Controllers/BasketControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Modals;
+using MyShop.Services;
+using MyShop.WebUI.Tests.Mocks;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class BasketControllerTests
+    {
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            //IRepository<BasketItem> basketItemContext = new MockContext<BasketItem>();
+            IRepository<Product> products= new MockContext<Product>();
+
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            basketService.AddToBasket(httpContext, "1");
+
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+
+
+        }
+    }
+}
